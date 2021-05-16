619a135e66478ec4e3621a1f0cf2a25187aa9bd8
Manan Gouhari <gouhari21@hotmail.com>
2021-05-16 22:11:38 +0530

    fixed DB modifier tags

--- graph/model/model.go
+++ graph/model/model.go
@@ -8,9 +8,9 @@ import (
 type Epoch struct {
 	tableName           struct{}               `pg:"epochs"`
 	ID                  string                 `pg:"default:gen_random_uuid()"`
-	StartBlock          int                    `pg:"not_null,use_zero,unique"`
-	EndBlock            int                    `pg:"not_null,use_zero,unique"`
-	Number              int                    `pg:"not_null,unique"`
+	StartBlock          int                    `pg:",not_null,use_zero,unique"`
+	EndBlock            int                    `pg:",not_null,use_zero,unique"`
+	Number              int                    `pg:",not_null,unique"`
 	CreatedAt           time.Time              `pg:"default:now()"`
 	ValidatorGroupStats []*ValidatorGroupStats `pg:"rel:has-many"`
 	ValidatorStats      []*ValidatorStats      `pg:"rel:has-many"`
@@ -18,10 +18,10 @@ type Epoch struct {
 
 type Validator struct {
 	ID               string    `pg:"default:gen_random_uuid()"`
-	Address          string    `pg:"not_null,unique"`
-	Name             string    `pg:"unique"`
+	Address          string    `pg:",not_null,unique"`
+	Name             string    `pg:",unique"`
 	CreatedAt        time.Time `pg:"default:now()"`
-	CurrentlyElected bool      `pg:"use_zero"`
+	CurrentlyElected bool      `pg:",use_zero"`
 	ValidatorGroup   *ValidatorGroup
 	Stats            []*ValidatorStats `pg:"rel:has-many"`
 	ValidatorGroupId string
@@ -33,27 +33,27 @@ func (v Validator) String() string {
 
 type ValidatorGroup struct {
 	ID                   string `pg:"default:gen_random_uuid()"`
-	Address              string `pg:"not_null,unique"`
-	Name                 string `pg:"unique"`
-	Email                string `pg:"unique"`
-	WebsiteURL           string `pg:"unique"`
-	DiscordTag           string `pg:"unique"`
-	TwitterUsername      string `pg:"unique"`
-	VerifiedDNS          bool   `pg:"use_zero"`
+	Address              string `pg:",not_null,unique"`
+	Name                 string `pg:",unique"`
+	Email                string `pg:",unique"`
+	WebsiteURL           string `pg:",unique"`
+	DiscordTag           string `pg:",unique"`
+	TwitterUsername      string `pg:",unique"`
+	VerifiedDNS          bool   `pg:",use_zero"`
 	GeographicLocation   string
 	CreatedAt            time.Time              `pg:"default:now()"`
-	EpochRegisteredAt    int                    `pg:"use_zero"`
-	EpochsServed         int                    `pg:"use_zero,default:0"`
-	RecievedVotes        int                    `pg:"use_zero"`
-	AvailableVotes       int                    `pg:"use_zero"`
-	GroupScore           int                    `pg:"use_zero"`
-	LockedCelo           int                    `pg:"use_zero"`
-	LockedCeloPercentile float64                `pg:"use_zero"`
-	SlashingPenaltyScore float64                `pg:"use_zero"`
-	AttestationScore     float64                `pg:"use_zero"`
-	EstimatedAPY         float64                `pg:"use_zero"`
-	TransparencyScore    float64                `pg:"use_zero"`
-	PerformanceScore     float64                `pg:"use_zero"`
+	EpochRegisteredAt    int                    `pg:",use_zero"`
+	EpochsServed         int                    `pg:",use_zero,default:0"`
+	RecievedVotes        int                    `pg:",use_zero"`
+	AvailableVotes       int                    `pg:",use_zero"`
+	GroupScore           int                    `pg:",use_zero"`
+	LockedCelo           int                    `pg:",use_zero"`
+	LockedCeloPercentile float64                `pg:",use_zero"`
+	SlashingPenaltyScore float64                `pg:",use_zero"`
+	AttestationScore     float64                `pg:",use_zero"`
+	EstimatedAPY         float64                `pg:",use_zero"`
+	TransparencyScore    float64                `pg:",use_zero"`
+	PerformanceScore     float64                `pg:",use_zero"`
 	Validators           []*Validator           `pg:"rel:has-many"`
 	Stats                []*ValidatorGroupStats `pg:"rel:has-many"`
 }
@@ -65,26 +65,26 @@ func (vg ValidatorGroup) String() string {
 type ValidatorGroupStats struct {
 	ID                    string `pg:"default:gen_random_uuid()"`
 	LockedCelo            string
-	LockedCeloPercentile  float64 `pg:"use_zero"`
-	GroupShare            string  `pg:"use_zero"`
-	Votes                 string  `pg:"use_zero"`
-	VotingCap             string  `pg:"use_zero"`
-	AttestationPercentage float64 `pg:"use_zero"`
-	SlashingScore         float64 `pg:"use_zero"`
+	LockedCeloPercentile  float64 `pg:",use_zero"`
+	GroupShare            string  `pg:",use_zero"`
+	Votes                 string  `pg:",use_zero"`
+	VotingCap             string  `pg:",use_zero"`
+	AttestationPercentage float64 `pg:",use_zero"`
+	SlashingScore         float64 `pg:",use_zero"`
 	Epoch                 *Epoch
 	EpochId               string
 	ValidatorGroup        *ValidatorGroup
 	ValidatorGroupId      string
 	CreatedAt             time.Time `pg:"default:now()"`
-	EstimatedAPY          float64   `pg:"use_zero"`
+	EstimatedAPY          float64   `pg:",use_zero"`
 }
 
 type ValidatorStats struct {
 	ID                     string `pg:"default:gen_random_uuid()"`
-	AttestationsRequested  int    `pg:"use_zero"`
-	AttenstationsFulfilled int    `pg:"use_zero"`
-	LastElected            int    `pg:"use_zero"`
-	Score                  string `pg:"use_zero"`
+	AttestationsRequested  int    `pg:",use_zero"`
+	AttenstationsFulfilled int    `pg:",use_zero"`
+	LastElected            int    `pg:",use_zero"`
+	Score                  string `pg:",use_zero"`
 	Epoch                  *Epoch
 	EpochId                string
 	Validator              *Validator
