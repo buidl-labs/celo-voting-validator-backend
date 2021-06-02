61199ce84c9b0872bfc263474271f54cf69c5c73
Manan Gouhari <gouhari21@hotmail.com>
2021-06-02 13:54:28 +0530

    Improved ValidateEmail function by also checking for MX records.

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"errors"
+	"net"
 	"regexp"
+	"strings"
 )
 
 func ValidateEmail(email string) (bool, error) {
@@ -15,8 +17,16 @@ func ValidateEmail(email string) (bool, error) {
 
 	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	doesMatch := emailRegex.MatchString(email)
-
 	if doesMatch {
+		emailParts := strings.Split(email, "@")
+		mx, err := net.LookupMX(emailParts[1])
+		if err != nil {
+			return false, err
+		}
+		if len(mx) == 0 {
+			return false, errors.New("no MX record found for the email domain")
+		}
+
 		return true, nil
 	} else {
 		return false, errors.New("email didn't pass pattern matching")
